d54b577f38ff646138572739599fa7e22ace84b1
Alexander Alarcon <walarcon14@gmail.com>
2019-06-05 20:03:02 -0500

    Update folders' structure

---
+++ src/containers/App/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      Hi from App
+    </div>
+  );
+}
+
+export default App;
